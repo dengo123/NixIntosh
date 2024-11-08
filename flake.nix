0a8af0b1726b4a464aed751a623ecd7d28aefeb2
Deniz Altiok <deniz060198@hotmail.com>
2024-11-08 12:45:58 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -1,117 +1,131 @@
 {
-  description = "Nix with Apples flake";
+  description = "Nix with Apple flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
-    
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
-  let
-    configuration = { pkgs, config,  ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      
-      environment.systemPackages =
-        [ pkgs.neovim
-          pkgs.mkalias
-	      pkgs.micro
-	      pkgs.alacritty
-	      pkgs.git
+  outputs = { self, nixpkgs, nix-darwin, nix-homebrew, ... }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-darwin"; };
+      lib = nixpkgs.lib;
+      configuration = { config, pkgs, ... }: {
+        networking.computerName = "MacBook von Deniz";
+        networking.hostName = "NixIntosh";
+
+        # Systemweite Pakete
+        environment.systemPackages = with pkgs; [
+          neovim
+          mkalias
+          micro
         ];
 
-	  homebrew = {
-	    enable = true;
-	    brews = [
-	      "mas"
-	    ];
-	    casks = [
-	      "wezterm"
-	    ];	
-	    masApps = {
-	      "Pages" = 409201541;
-	      "Numbers" = 409203825;
-	      "Keynote" = 409183694;
-	      "GarageBand" = 682658836;
-	    };
-	    onActivation.cleanup = "zap";
-	  };
-	  
-      fonts.packages = [
-      	(pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
-      ];
+        # Homebrew deklarative Verwaltung
+        homebrew = {
+          enable = true;
+          brews = [ "mas" ];
+          casks = [ "wezterm" "zen-browser" "wine-stable" "zoom" ];
+          masApps = {
+            "Pages" = 409201541;
+            "Numbers" = 409203825;
+            "Keynote" = 409183694;
+            "GarageBand" = 682658836;
+          };
+          onActivation = {
+            cleanup = "zap";
+            autoUpdate = true;
+            upgrade = true;
+          };
+        };
 
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
+        # Fonts
+        fonts.packages = [
+          (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+        ];
 
-      # Enable spotlight search for Nix apps
-	  system.activationScripts.applications.text = let
-	    env = pkgs.buildEnv {
-	      name = "system-applications";
-	      paths = config.environment.systemPackages;
-	      pathsToLink = "/Applications";
-	    };
-	  in
-	    pkgs.lib.mkForce ''
-	    # Set up applications.
-	    echo "setting up /Applications..." >&2
-	    rm -rf /Applications/Nix\ Apps
-	    mkdir -p /Applications/Nix\ Apps
-	    find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-	    while read src; do
-	      app_name=$(basename "$src")
-	      echo "copying $src" >&2
-	      ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-	    done
-	        '';
-	        
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+        # Nix-Daemon Service und Nix Einstellungen
+        services.nix-daemon.enable = true;
+        nix.settings.experimental-features = [ "nix-command" "flakes" ];
+        nixpkgs.config.allowUnfree = true;
 
-	  # Eanble unfree software
-      nixpkgs.config.allowUnfree = true;
+        # Spotlight für Nix-Apps
+        system.activationScripts.applications.text = let
+          env = pkgs.buildEnv {
+            name = "system-applications";
+            paths = config.environment.systemPackages;
+          };
+        in
+        pkgs.lib.mkForce ''
+          echo "Setting up /Applications..." >&2
+          rm -rf /Applications/Nix\ Apps
+          mkdir -p /Applications/Nix\ Apps
+          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+          while read src; do
+            app_name=$(basename "$src")
+            echo "Copying $src" >&2
+            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+          done
+        '';
 
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
+        # Aktivierungsskript für Berechtigungen hinzu
+        system.activationScripts.fixPermissions.text = ''
+          echo "Fixing /nix permissions..."
+          sudo chown -R root:wheel /nix
+        '';
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
+        # macOS Systemeinstellungen
+        system.defaults = {
+          dock = { 
+            autohide = true;
+            persistent-apps = [
+              "/System/Applications/Mail.app"
+              "/System/Applications/Calendar.app"
+              "/System/Applications/Notes.app"
+              "/Applications/Zen Browser.app"
+              "/Applications/WezTerm.app"
+            ];
+            show-recents = false;
+          };
+          finder = {
+            FXPreferredViewStyle = "clmv";
+            _FXShowPosixPathInTitle = true;
+            AppleShowAllFiles = true;
+          };
+          loginwindow.GuestEnabled = false;
+          NSGlobalDomain = {
+            AppleInterfaceStyle = "Dark";
+            AppleShowAllFiles = true;
+            NSWindowShouldDragOnGesture = true;
+          };
+          SoftwareUpdate.AutomaticallyInstallMacOSUpdates = false;  
+        };
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
+        # Versionsnummer und State Version
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+        system.stateVersion = 5;
+      };
+    in {
+      darwinConfigurations.NixIntosh = nix-darwin.lib.darwinSystem {
+        system = "x86_64-darwin";
+        modules = [
+          configuration
+          (nix-homebrew.darwinModules.nix-homebrew {
+            lib = lib;
+            nix-homebrew = {
+              enable = true;
+              enableRosetta = false;
+              user = "dengo123";
+              autoMigrate = false;
+            };
+          })
+        ];
+      };
 
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "x86_64-darwin";
+      darwinPackages = self.darwinConfigurations.NixIntosh.pkgs;
     };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#NixIntosh
-    darwinConfigurations."Nixintosh" = nix-darwin.lib.darwinSystem {
-      modules = [
-        configuration
-	    nix-homebrew.darwinModules.nix-homebrew
-	    {
-	  	  nix-homebrew = {
-	    # Install Homebrew under the default prefix
-	      enable = true; 
-	    # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-	      enableRosetta = false;
-	    # User owning the Homebrew prefix
-	      user = "dengo123";
-	    # Automatically migrate existing Homebrew installations
-	      autoMigrate = false;
-	      };
-        }
-      ];
-    };
-
-    # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations."Nixintosh".pkgs;
-  };
 }
+
+
